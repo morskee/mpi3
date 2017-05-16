af3832eeb6760e3ca68719db3eb143df97bb5ddd
master <kammoren@icloud.com>
2017-05-16 22:35:38 +0200

    packed

--- task3.c
+++ task3.c
@@ -6,22 +6,22 @@
 #include <time.h>
 #include <math.h>
 
-double evaluate_f_of_x(double alfa, double k)
-      {
-        double x;
+double evaluate_f_of_x(double x) {
+        int k;
+        float coeffs[100];
+        double f_of_x;
         for (size_t i = 0; i < k; i++) {
-          x = alfa * pow(x, k);
+          f_of_x = f_of_x + (coeffs[i] + pow(x, i));
         }
+        return f_of_x;
       }
 
-double integrate(double a, double b, int m)
-       {
+double integrate(double a, double b, int m) {
          double x;
          double f_of_x;
          double h = ( b - a ) / m;
          double integral = 0.0;
-         for (int i = 0; i <= m; i++)
-         {
+         for (int i = 0; i <= m; i++) {
            x = a + i * h;
            f_of_x = evaluate_f_of_x(x);
            if ( ( i == 0 ) || ( i == m ) )
@@ -37,7 +37,7 @@ int isNumberCharacter (char asciiCharacter) {
   if ((asciiCharacter >= 48 && asciiCharacter <= 57) ||
     asciiCharacter == 45 || asciiCharacter == 46) {
     return 1;
-  } else{
+  } else {
     return 0;
   }
 }
@@ -45,7 +45,7 @@ int isNumberCharacter (char asciiCharacter) {
 int isEndLineCharacter (char asciiCharacter) {
   if (asciiCharacter == 10 || asciiCharacter == 13) {
     return 1;
-  } else{
+  } else {
     return 0;
   }
 }
@@ -55,7 +55,7 @@ int getSingleIntFromLine(FILE *file) {
   int size = 0;
   char readAsciiCharacter = 0;
 
-  while ((readAsciiCharacter = getc(file)) != EOF && isNumberCharacter(readAsciiCharacter)){
+  while ((readAsciiCharacter = getc(file)) != EOF && isNumberCharacter(readAsciiCharacter)) {
     cache[size] = readAsciiCharacter;
     size++;
   }
@@ -63,8 +63,7 @@ int getSingleIntFromLine(FILE *file) {
 }
 
 void cleanTable (char *table) {
-  for (int i = 0; i < 100; ++i)
-  {
+  for (int i = 0; i < 100; ++i) {
     table[i] = 0;
   }
 }
@@ -76,17 +75,17 @@ int getTableOfArguments(FILE *file, float *array) {
       cacheSize = 0,
       asciiCharacter = 0;
 
-  while ((asciiCharacter = getc(file)) != EOF || asciiCharacter == 10){
-          if (isNumberCharacter(asciiCharacter)){
+  while ((asciiCharacter = getc(file)) != EOF || asciiCharacter == 10) {
+          if (isNumberCharacter(asciiCharacter)) {
             cache[cacheSize] = asciiCharacter;
             cacheSize ++;
           }
-          if (asciiCharacter == 32 || isEndLineCharacter(asciiCharacter)){
+          if (asciiCharacter == 32 || isEndLineCharacter(asciiCharacter)) {
             array[arraySize] = atof(cache);
             arraySize ++;
             cacheSize = 0;
             cleanTable(cache);
-            if (isEndLineCharacter(asciiCharacter)){
+            if (isEndLineCharacter(asciiCharacter)) {
               break;
             }
          }
@@ -102,13 +101,13 @@ void getInterval(FILE *file, float *start, float *end) {
   int secondArgument = 0,
       cacheSize = 0,
       asciiCharacter = 0;
-  while ((asciiCharacter = getc(file)) != EOF || asciiCharacter == 10){
-          if (isNumberCharacter(asciiCharacter)){
+  while ((asciiCharacter = getc(file)) != EOF || asciiCharacter == 10) {
+          if (isNumberCharacter(asciiCharacter)) {
             cache[cacheSize] = asciiCharacter;
             cacheSize ++;
           }
-          if (asciiCharacter == 32 || isEndLineCharacter(asciiCharacter)){
-            if (secondArgument){
+          if (asciiCharacter == 32 || isEndLineCharacter(asciiCharacter)) {
+            if (secondArgument) {
               endInterval = atof(cache);
               break;
             } else {
@@ -123,8 +122,7 @@ void getInterval(FILE *file, float *start, float *end) {
   *end = endInterval;
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     MPI_Init(&argc, &argv);
 
     int rank, size;
@@ -138,25 +136,24 @@ int main(int argc, char **argv)
           endInterval = 0;
     int asciiCharacter = 0,
         coeffsSize = 0;
+    int k, integration;
 
     FILE *file;
     file = fopen("test.txt", "r");
     if (file) {
         while ((asciiCharacter = getc(file)) != EOF){
           if ((asciiCharacter >= 'a' && asciiCharacter <= 'z') || asciiCharacter == 32) {
-            if (asciiCharacter == 32)
-            {
-              switch(parametersTypeCache)
-              {
+            if (asciiCharacter == 32) {
+              switch(parametersTypeCache) {
                 case 'e':
                   printf("degree:\n");
-                  printf("%i\n", getSingleIntFromLine(file));
+                  k = getSingleIntFromLine(file);
+                  printf("%i\n", k);
                 break;
                 case 's':
                   printf("coeffs:\n");
                   coeffsSize = getTableOfArguments(file, coeffs);
-                  int i;
-                  for (i = 0; i < coeffsSize; i ++) {
+                  for (size_t i = 0; i < coeffsSize; i ++) {
                       printf("%lf\n", coeffs[i]);
                   }
                 break;
@@ -168,7 +165,8 @@ int main(int argc, char **argv)
                 break;
                 case 'n':
                   printf("integration:\n");
-                  printf("%i\n", getSingleIntFromLine(file));
+                  integration = getSingleIntFromLine(file);
+                  printf("%i\n", integration);
                 break;
                 default:
                   printf("none");
@@ -178,8 +176,55 @@ int main(int argc, char **argv)
          }
         }
     }
-    float calka;
-    calka = integrate(startInterval, endInterval, getSingleIntFromLine(file));
+    //float calka;
+    //calka = integrate(startInterval, endInterval, integration);
+    //printf ("%f\n", calka);
+
+    int pack_size;
+    char *pack_buff;
+    int pack_position;
+    int all_floats = k + 3;
+    int all_ints = 2;
+
+    if (rank == 0) {
+      int tmp_pack_size;
+
+      MPI_Pack_size(all_ints, MPI_INT, MPI_COMM_WORLD, &tmp_pack_size);
+      pack_size = tmp_pack_size;
+
+      MPI_Pack_size(all_floats, MPI_FLOAT, MPI_COMM_WORLD, &tmp_pack_size);
+      pack_size += tmp_pack_size;
+
+      pack_buff = new char [pack_size];
+
+      pack_position = 0;
+      MPI_Pack(&k, all_ints, MPI_INT, pack_buff, pack_size, &pack_position, MPI_COMM_WORLD);
+      MPI_Pack(&coeffs[0], all_floats, MPI_FLOAT, pack_buff, pack_size, &pack_position, MPI_COMM_WORLD);
+
+      MPI_Bcast(%pack_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
+      MPI_Bcast(pack_buff, pack_size, MPI_PACKED, 0, MPI_COMM_WORLD);
+    } else {
+      MPI_Bcast(&pack_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
+      pack_buff = new char [pack_size];
+      MPI_Bcast(pack_buff, pack_size, MPI_PACKED, 0, MPI_COMM_WORLD);
+
+      pack_position = 0;
+      MPI_Unpack(pack_buff, pack_size, &pack_position, &k, all_ints, MPI_INT, MPI_COMM_WORLD);
+      MPI_Unpack(pack_buff, pack_size, &pack_position, &coeffs[0], all_floats, MPI_FLOAT, MPI_COMM_WORLD);
+    }
+
+    //dekompozycja
+    int p = m / n;
+    int r = m % n;
+    int my_number_of_subintervals;
+    int my_first_midpoint;
+    if (rank < r) {
+      my_number_of_subintervals = p + 1;
+      my_first_midpoint = rank * (p + 1);
+    } else {
+      my_number_of_subintervals = p;
+      my_first_midpoint = r * (p + 1) + (rank - r) * p;
+    }
 
     MPI_Finalize();
     return 0;
